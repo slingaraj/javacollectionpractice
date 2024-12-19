718daf234192edee1f23a84700001522a2c7c0dd
lingaraj <lingaraj@suyogindia.com>
2024-12-19 14:26:59 +0530

    chnages

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Vector;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Vector<Integer> vector= new Vector<>(11);
-        System.out.println(vector.capacity());//default is 10 else o/p 11
+        HashMap<Integer,String> map= new HashMap<>();
+        map.put(1,"Hasrshit");
+        map.put(2,"Lingaraj");
+        map.put(3,"Neha");
+//        System.out.println(map);
 
 
-
-
-
-
-
-
-
-
+        Set<Map.Entry<Integer,String>> entries= map.entrySet();
+        for (Map.Entry<Integer, String> entry:entries){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+            entry.setValue(entry.getValue().toUpperCase());
+        }
+        System.out.println(map);
 
     }
 }
